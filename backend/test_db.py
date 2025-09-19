bb06f2b14a7e97dba62db6734e828f644060599f
Mayra Harley <39833685+reservecit@users.noreply.github.com>
2025-09-19 16:55:30 -0400

    Update test_db.py
    updated exception message

--- backend/test_db.py
+++ backend/test_db.py
@@ -3,5 +3,9 @@ from sqlalchemy import create_engine
 DATABASE_URL = "postgresql+psycopg2://postgres:sMc110499meNcp@127.0.0.1/pmd_dev"
 engine = create_engine(DATABASE_URL)
 
-with engine.connect() as conn:
-    print("Connected successfully!")
+try:
+    with engine.connect() as conn:
+        print("Connected successfully!")
+except Exception as e:
+    print("Connection failed:")
+    print(e)
