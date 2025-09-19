becf77e9423da52d204b011e404f81550bee56f8
Mayra Harley <39833685+reservecit@users.noreply.github.com>
2025-09-19 16:08:41 -0400

    Update database.py
    Swapped localhost to IP

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace with your actual DSN or connection string
-DATABASE_URL = "postgresql+psycopg2://postgres:D%5BJMHZ%5B%243t0%23ZEs%40@localhost/PMD_Dev"
+DATABASE_URL = "postgresql+psycopg2://postgres:D%5BJMHZ%5B%243t0%23ZEs%40@127.0.0.1/PMD_Dev"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
