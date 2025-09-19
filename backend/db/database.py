8885bec162ed0fc1bc04607583edbc9d5439da7b
Mayra Harley <39833685+reservecit@users.noreply.github.com>
2025-09-19 14:25:36 -0400

    Update database.py
    add test db

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace with your actual DSN or connection string
-DATABASE_URL = "postgresql+psycopg2://username:password@localhost/PMD_Dev"
+DATABASE_URL = "postgresql+psycopg2://postgres:D[JMHZ[$3tO#ZEs@@localhost/PMD_Dev"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
