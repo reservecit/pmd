c38f2326c02d1d4c843a60d771fc3857ff73e0e9
Mayra Harley <39833685+reservecit@users.noreply.github.com>
2025-09-19 15:32:16 -0400

    Update main.py
    Added url and port number

--- backend/main.py
+++ backend/main.py
@@ -19,3 +19,7 @@ app.add_middleware(
 
 # Include routes
 app.include_router(profiles.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
