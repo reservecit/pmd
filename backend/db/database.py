93b22c76d73390558d3e04509de9600e85bb2343
Mayra Harley <39833685+reservecit@users.noreply.github.com>
2025-09-19 16:34:00 -0400

    Update database.py
    Correct PW

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace with your actual DSN or connection string
-DATABASE_URL = "postgresql+psycopg2://postgres:D%5BJMHZ%5B%243t0%23ZEs%40@127.0.0.1/pmd_dev"
+# DATABASE_URL = "postgresql+psycopg2://postgres:D%5BJMHZ%5B%243t0%23ZEs%40@127.0.0.1/pmd_dev"
+DATABASE_URL = "postgresql+psycopg2://postgres:D%5BJMHZ%5B%243t0%23ZEs%21@127.0.0.1/pmd_dev"
 # DATABASE_URL = "postgresql+psycopg2://postgres:your_encoded_password@127.0.0.1/pmd_dev"
 
 engine = create_engine(DATABASE_URL)
