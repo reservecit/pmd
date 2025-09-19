c7f9bd18c3f380411b69f81c63523a4307913667
Mayra Harley <39833685+reservecit@users.noreply.github.com>
2025-09-19 14:23:07 -0400

    Create database.py
    Create db connection

---
+++ backend/db/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Replace with your actual DSN or connection string
+DATABASE_URL = "postgresql+psycopg2://username:password@localhost/PMD_Dev"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
